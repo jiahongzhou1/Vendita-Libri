d286c3c8049fcd801cfb9cbd0853412c31a53455
jiahong zhou <jiahong.zhou@marconirovereto.it>
2023-10-18 21:42:21 +0200

    aggiunto pagina-intro

---
+++ Vendita-Libri/src/app/paginaIntro/pagina-iniziale/pagina-iniziale.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-iniziale',
+  templateUrl: './pagina-iniziale.component.html',
+  styleUrls: ['./pagina-iniziale.component.css']
+})
+export class PaginaInizialeComponent {
+
+}
